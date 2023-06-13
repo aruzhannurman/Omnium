6d52e8dfceaeba986195c756c3c5c3811af74f69
Aruzhan Nurman <94863265+aruzhannurman@users.noreply.github.com>
2023-06-13 21:08:24 +0600

    menuItemsModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,17 @@
 import { Component } from '@angular/core';
-
+import { MenuItemModel } from './components/menu/models/MenuItemModel';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-
-
 export class AppComponent {
   title = 'second';
 
-  items=[
-    {
-    title:'Users',
-    icon:'./assets/link.png'
-  },
-  {
-    title:'Features',
-    icon:'./assets/link.png'
-  },
-  {
-    title:'Main',
-    icon:'./assets/link.png'
-  },
-]
-
-
-
+  items: MenuItemModel[] = [
+    new MenuItemModel('Users1', './assets/link.png'),
+    new MenuItemModel('Users2', './assets/link.png'),
+    new MenuItemModel('Users3', './assets/link.png'),
+  ];
 }
 
